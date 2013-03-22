023db476d1172b61997433b722671b1abf71084d
otternq <otternq@gmail.com>
2013-03-21 20:14:01 -0700

    #1 unit test for add_group

--- src/com/nickotter/randomname/test/DatabaseCRUDTest.java
+++ src/com/nickotter/randomname/test/DatabaseCRUDTest.java
@@ -1,39 +1,45 @@
 package com.nickotter.randomname.test;
 
+import junit.framework.TestCase;
+
 import com.nickotter.randomname.CRUD;
+import com.nickotter.randomname.Group;
 import com.nickotter.randomname.Sqlite;
 
 import android.content.ContentProvider;
 import android.content.Context;
-import android.test.ProviderTestCase2;
+
+import android.test.AndroidTestCase;
+import android.test.mock.MockContext;
 import android.util.Log;
 
-public class DatabaseCRUDTest extends ProviderTestCase2<ContentProvider> {
+public class DatabaseCRUDTest extends AndroidTestCase {
 	
 	private String LOGTAG = "DatabaseCRUDTest";
 	
 	Context context = null;
 	CRUD databaseCRUD = null;
-
-	public DatabaseCRUDTest()
-    {
-        this(DatabaseCRUDTest.class, "my_authority_from_manifest");
-    }
 	
-	public DatabaseCRUDTest(Class<ContentProvider> providerClass, String providerAuthority) {
-		super(providerClass, providerAuthority);
-		// TODO Auto-generated constructor stub
-		
-		this.context = getMockContext();
-	}
-	
-	public void test_InitializingCRUD() {
+	public void setUp() {
 		Log.v(LOGTAG, "test_InitializingCRUD e");
 
 		Log.v(LOGTAG, "Initializing CRUD object");
-		this.databaseCRUD = new CRUD(this.context);
+		
+		this.databaseCRUD = new CRUD(getContext());
+		this.databaseCRUD.open();
 		
 		Log.v(LOGTAG, "test_InitializingCRUD x");
+	}
+	
+	public void tearDown() {
+		this.databaseCRUD.close();
+	}
+	
+	public void testAddGroup() {
+		Group temp = new Group("CS480");
+		this.databaseCRUD.add_group(temp);
+		
+		assertEquals(1, temp.getID());
 	}
 
 }
