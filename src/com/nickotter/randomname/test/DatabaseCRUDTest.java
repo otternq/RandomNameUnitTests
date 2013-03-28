55abab1efff844e22586d66db76a86e7dc75ba1d
otternq <otternq@gmail.com>
2013-03-28 12:52:24 -0700

    adding android tests for adding lists and quering groups

--- src/com/nickotter/randomname/test/DatabaseCRUDTest.java
+++ src/com/nickotter/randomname/test/DatabaseCRUDTest.java
@@ -1,16 +1,14 @@
 package com.nickotter.randomname.test;
 
-import junit.framework.TestCase;
+import java.util.List;
 
 import com.nickotter.randomname.CRUD;
 import com.nickotter.randomname.Group;
+import com.nickotter.randomname.MyList;
 import com.nickotter.randomname.Sqlite;
-
-import android.content.ContentProvider;
 import android.content.Context;
 
 import android.test.AndroidTestCase;
-import android.test.mock.MockContext;
 import android.util.Log;
 
 public class DatabaseCRUDTest extends AndroidTestCase {
@@ -19,7 +17,7 @@ public class DatabaseCRUDTest extends AndroidTestCase {
 	
 	Context context = null;
 	CRUD databaseCRUD = null;
-	
+
 	public void setUp() {
 		Log.v(LOGTAG, "setUp e");
 		
@@ -38,9 +36,43 @@ public class DatabaseCRUDTest extends AndroidTestCase {
 		this.databaseCRUD.close();
 	}
 	
+	//GROUP TESTS
+	
+	/**
+	 * Tests that databaseCRUd can insert a Group
+	 */
 	public void testAddGroup() {
 		Group temp = new Group("CS480");
 		this.databaseCRUD.add_group(temp);
+		
+		assertEquals(1, temp.getID());
+	}
+	
+	/**
+	 * Tests that databaseCRUD can find Groups
+	 */
+	public void testQueryGroup() {
+		List<Group> groupList = this.databaseCRUD.query_group();
+		
+		String[] expectedName = {"CS480"};
+		int[] expectedID = {1};
+		
+		for (int i = 0; i < groupList.size(); i++) {
+			assertEquals(expectedID[i], groupList.get(i).getID()); //tests that it has the expected id
+			assertEquals(expectedName[i], groupList.get(i).getName()); //tests that it has the expected name
+		}
+	}
+	
+	//List Test
+	
+	public void testAddList() {
+		
+		Group tempGroup = new Group("CS480");
+		this.databaseCRUD.add_group(tempGroup);
+		
+		MyList temp = new MyList(0, tempGroup.getID(), "Test List Name");
+		
+		databaseCRUD.add_list(tempGroup, temp); //add the list to the database
 		
 		assertEquals(1, temp.getID());
 	}
