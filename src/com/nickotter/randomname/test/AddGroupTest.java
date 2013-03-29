f71144239eb9103824cf35035d5167f17c656148
otternq <otternq@gmail.com>
2013-03-28 18:33:39 -0700

    closes #2.  Group Test found the group added by the unit test

--- src/com/nickotter/randomname/test/AddGroupTest.java
+++ src/com/nickotter/randomname/test/AddGroupTest.java
@@ -1,34 +1,75 @@
 package com.nickotter.randomname.test;
 
+import java.util.List;
+
+import android.content.Context;
 import android.test.ActivityInstrumentationTestCase2;
+import android.util.Log;
 import android.widget.EditText;
 
 import com.jayway.android.robotium.solo.Solo;
+import com.nickotter.randomname.CRUD;
+import com.nickotter.randomname.Group;
+import com.nickotter.randomname.MainActivity;
 import com.nickotter.randomname.crudActivities.AddGroup;
 
-public class AddGroupTest extends ActivityInstrumentationTestCase2<AddGroup> {
+public class AddGroupTest extends ActivityInstrumentationTestCase2<MainActivity> {
 	
 	private Solo solo;
 	
+	Context context= null;
+	CRUD databaseCRUD = null;
+	
 	public AddGroupTest() {
-		super(AddGroup.class);
+		super(MainActivity.class);
 	
 	}
 	
 	protected void setUp() throws Exception {
 		super.setUp();
 		solo = new Solo(getInstrumentation(), getActivity());
+		
+		this.context = this.getInstrumentation().getTargetContext().getApplicationContext();
+		
+		this.databaseCRUD = new CRUD(this.context);
+		this.databaseCRUD.open();
 	}
 	
 	public void testAddGroup() {
 		
-		EditText groupName = (EditText) solo.getEditText("Enter Group Name");
+		solo.assertCurrentActivity("The App should launch", MainActivity.class);
+		solo.assertCurrentActivity("This test should start at the MainActivity Activity", MainActivity.class);
 		
-		solo.clearEditText(groupName);
+		//switch to the AddGroup Activity
+		solo.clickOnImage(0);
+		solo.clickOnButton("Add Group");
 		
-		groupName.setText("Unit Test Group Name");
+		solo.assertCurrentActivity("This test should have changed to the AddGroup Activity", AddGroup.class);
 		
-		solo.clickOnButton("Cancel");
+		solo.clearEditText(0);
+		solo.enterText(0, "Unit Test Group Name");
+		
+		solo.clickOnButton("Save");
+		
+		//switch to the MainActivity Activity
+		solo.waitForActivity("MainActivity");
+		solo.assertCurrentActivity("The App should go back to the MainActivity", MainActivity.class);
+		
+		List<Group> groupList = this.databaseCRUD.query_group();
+		
+		boolean found = false;
+		
+		Log.v("testAddGroup", "found this many groups:" + groupList.size());
+		
+		for (Group temp: groupList) {
+			Log.v("testAddGroup", "found group: " + temp.getName());
+			if (temp.getName().equals("Unit Test Group Name")) {
+				found = true;
+				Log.v("testAddGroup", "it has been found");
+			}
+		}
+		
+		assertTrue(found);
 	}
 
 
